7971cd305b8062a162331a783001ec7e3ac72ba5
orhan34 <orhan34@outlook.com>
2015-06-27 03:01:10 +0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -961,7 +961,7 @@ console.log("Running Bot Launcher!");
   dArc = [],
   dText = [],
   lines = [],
-  names = ["Agarion.web.tr"],
+  names = ["Agario.web.tr"],
   originalName = names[Math.floor(Math.random() * names.length)],
   sessionScore = 0,
   serverIP = "",
